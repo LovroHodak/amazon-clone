d80beb0fa0f45df2acda66cf34793d92ca5b2808
Lovro <hodak.lovro@gmail.com>
2020-11-27 17:57:29 +0100

    startin with redux

---
+++ amazon-clone/src/components/Checkout.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Checkout.css'
+import Subtotal from './Subtotal'
+
+function Checkout() {
+    return (
+        <div className='checkout' >
+            <div className='checkout__left' >
+                <img 
+                    className='checkout__ad' 
+                    alt='checkout-ad'
+                    src='https://i.imgur.com/8Wje1hr.jpg' 
+                    />
+
+                    <div>
+                        <h2 className='checkout__title' >Your shopping Basket</h2>
+                        {/* Basket Item */}
+                        {/* Basket Item */}
+                        {/* Basket Item */}
+                        {/* Basket Item */}
+                    </div>
+            </div>
+            <div className='checkout__right' >
+                <Subtotal />
+            </div>
+        </div>
+    )
+}
+
+export default Checkout
