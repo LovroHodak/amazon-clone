ed138962de7d576b4f2b17eb200d6d0d4e420a9f
Lovro <hodak.lovro@gmail.com>
2020-12-01 18:03:45 +0100

    checkout functionalities

--- amazon-clone/src/components/Subtotal.js
+++ amazon-clone/src/components/Subtotal.js
@@ -2,8 +2,9 @@ import React from 'react'
 import './Subtotal.css'
 import CurrencyFormat from 'react-currency-format'
 import { useStateValue } from '../StateProvider'
+import { getBasketTotal } from '../Reducer'
 
-function Subtotal({price}) {
+function Subtotal() {
     const [{basket}, dispatch] = useStateValue()
     return (
         <div className='subtotal' >
@@ -11,7 +12,7 @@ function Subtotal({price}) {
                 renderText={(value) => (
                     <>
                         <p>
-                            Subtotal ({basket?.length} items): <strong>0 </strong>
+                            Subtotal ({basket.length} items): <strong>{value}</strong>
                         </p>
                         <small className='subtotal__gift' >
                             <input type='checkbox' />This order contains a gift
@@ -19,7 +20,7 @@ function Subtotal({price}) {
                     </>
                 )}
                 decimalScale={2}
-                value={0}
+                value={getBasketTotal(basket)}
                 displayType={'text'}
                 thousandSeparator={true}
                 prefix={'$'}
