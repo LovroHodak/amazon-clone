18425245c26ffd55083b5b2537b41da38e6a8aae
Lovro <hodak.lovro@gmail.com>
2020-12-02 18:29:37 +0100

    payment

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -7,6 +7,7 @@ import Checkout from './components/Checkout'
 import Login from './components/Login'
 import { auth } from './firebase'
 import { useStateValue } from './StateProvider'
+import Payment from './components/Payment'
 
 function App() {
   const [{}, dispatch] = useStateValue()
@@ -46,6 +47,11 @@ function App() {
           <Route path='/checkout' >
             <Header />
             <Checkout />
+          </Route>
+
+          <Route path='/payment' >
+            <Header />
+            <Payment />
           </Route>
           
           <Route path='/' >
