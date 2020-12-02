62b539b95bb736b66ff8ef169dcd66adbaa57d4b
Lovro <hodak.lovro@gmail.com>
2020-12-02 10:13:39 +0100

    before comp crashes

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -4,20 +4,27 @@ import Header from './components/Header'
 import Home from './components/Home'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Checkout from './components/Checkout'
+import Login from './components/Login'
 
 function App() {
   return (
     //BEM
     <Router>
-      <div className="App">
-        <Header />
+      <div className="app">
+        
 
         <Switch>
+          <Route path='/login' >
+            <Login />
+          </Route>
+
           <Route path='/checkout' >
+            <Header />
             <Checkout />
           </Route>
           
           <Route path='/' >
+            <Header />
             <Home />
           </Route>
         </Switch>
